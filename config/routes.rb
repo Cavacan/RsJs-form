44dc213185eb556b0cd50ae8d2eacd4bb15a2fdc
cvcn <tgenk0901@gmail.com>
2024-10-11 10:29:29 +0900

    add tagsIndex

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  get 'tags', to: 'tags#index'
   get 'rs_home/index', to: 'rs_home#index'
   get 'js_home/index', to: 'js_home#index'
   get 'js_home/get_form', to: 'js_home#get_form'
